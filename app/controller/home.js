7f1f53ff4b73ec78572c36d61a749cde98220376
hqchqc <734986933@qq.com>
2021-12-08 21:15:52 +0800

    feat: 参考下掘金大佬的代码

--- app/controller/home.js
+++ app/controller/home.js
@@ -7,6 +7,14 @@ class HomeController extends Controller {
     const { ctx } = this;
     ctx.body = 'hi, egg';
   }
+  async send() {
+    const { ctx, app } = this;
+    ctx.body = app.config;
+    const result = await ctx.service.sendmsg.sendOut();
+    ctx.logger.info('主动触发，发送模板消息 结果: %j', result);
+    ctx.body = result;
+    ctx.set('Content-Type', 'application/json');
+  }
 }
 
 module.exports = HomeController;
