8a57c7fd9e3c587d789219ea56c0f5d5d8ba63ca
hqchqc <734986933@qq.com>
2021-12-09 20:22:07 +0800

    feat: 添加key

--- config/config.default.js
+++ config/config.default.js
@@ -17,6 +17,41 @@ module.exports = appInfo => {
 
   // add your middleware config here
   config.middleware = [];
+  // 天气接口配置
+  config.weather = {
+    appid: '*****************',
+    appsecret: '************',
+  };
+  // 测试 微信公众号
+  config.weChat = {
+    appld: 'wxf9a7417fc1598cdc',
+    secret: '*****',
+    // 用户的openid
+    users: [
+      'ocSwk5qe173gL5ozcPQbTuoFfUKk',
+    ],
+    daily: '********************', // 普通模板
+    marry: '********************', // 结婚纪念日模板
+    wageDay: '*****************', // 工资日模板
+    birthday: '**************************', // 生日模板
+  };
+
+  // 时间
+  config.time = {
+    wageDay: 15, // 工资日
+    love: '2017-06-09', // 相爱日期
+    marry: '2020-11-27', // 结婚纪念日
+    // 生日配置
+    birthday: {
+      2021: '2021-04-21',
+      2022: '2022-01-06',
+      2023: '2023-02-25',
+      2024: '2024-03-14',
+      2025: '2025-04-03',
+      2026: '2026-05-22',
+    }, // 每年生日 阳历
+    birthYear: 1995,
+  };
 
   // add your user config here
   const userConfig = {
